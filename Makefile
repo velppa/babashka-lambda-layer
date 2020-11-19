24c25833b8ec63190b67c75dbb53d7178b57ed13
Pavel Popov <pavel@findhotel.net>
2020-11-19 12:35:00 +0100

    Improve error reporting

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
+GIT_COMMIT = $(shell git rev-parse --short HEAD)
+
 build:
 	docker build --target BUILDER -t babashka-lambda-archiver .
 	docker rm build || true
 	docker create --name build babashka-lambda-archiver
 	docker cp build:/var/task/babashka-runtime.zip babashka-runtime.zip
 
-publish-layer: build
+deploy: build
 	aws lambda publish-layer-version \
 		--layer-name babashka-runtime \
+		--description "commit $(GIT_COMMIT)" \
 		--compatible-runtimes provided.al2 \
 		--zip-file fileb://babashka-runtime.zip
--- src/lambda/impl/runtime.clj
+++ src/lambda/impl/runtime.clj
@@ -3,43 +3,50 @@
     [cheshire.core :as json]
     [clj-http.lite.client :as http]))
 
+
 (defn- get-lambda-invocation-request [runtime-api]
   (http/request
-    {:method   :get
-     :url      (str "http://" runtime-api "/2018-06-01/runtime/invocation/next")
-     :timeout  900000}))
+   {:method   :get
+    :url      (str "http://" runtime-api "/2018-06-01/runtime/invocation/next")
+    :timeout  900000}))
 
-(defn- send-response [runtime-api lambda-runtime-aws-request-id response-body]
-  (http/request
-    {:method  :post
-     :url     (str "http://" runtime-api "/2018-06-01/runtime/invocation/" lambda-runtime-aws-request-id "/response")
-     :body    response-body
-     :headers {"Content-Type" "application/json"}}))
 
-(defn- send-error [runtime-api lambda-runtime-aws-request-id error-body]
+(defn- send-response [runtime-api request-id response-body]
   (http/request
-    {:method  :post
-     :url     (str "http://" runtime-api "/2018-06-01/runtime/invocation/" lambda-runtime-aws-request-id "/error")
-     :body    error-body
-     :headers {"Content-Type" "application/json"}}))
+   {:method  :post
+    :url     (str "http://" runtime-api "/2018-06-01/runtime/invocation/" request-id "/response")
+    :body    response-body
+    :headers {"Content-Type" "application/json"}}))
+
+
+(defn- send-error [runtime-api request-id error-body]
+  (http/request
+   {:method  :post
+    :url     (str "http://" runtime-api "/2018-06-01/runtime/invocation/" request-id "/error")
+    :body    {:errorType (str (or (:error error-body) "unknown"))
+              :errorMessage (str (when (:error error-body)
+                                   (dissoc error-body :error)
+                                   error-body))}
+    :headers {"Content-Type" "application/json"}}))
+
 
 (defn- request->response [request-body handler-fn context]
   (let [decoded-request (json/decode request-body true)
         event (or (-> decoded-request :body (json/decode true)) decoded-request)]
     (handler-fn event (into {} context))))
 
+
 (defn init [handler-fn context]
   (let [runtime-api (System/getenv "AWS_LAMBDA_RUNTIME_API")]
     (loop [req (get-lambda-invocation-request runtime-api)]
-      (let [lambda-runtime-aws-request-id (get (get req :headers) "lambda-runtime-aws-request-id")]
+      (let [request-id (get (get req :headers) "lambda-runtime-aws-request-id")]
         (when-let [error (get req :error)]
-          (send-error runtime-api lambda-runtime-aws-request-id (str error))
+          (send-error runtime-api request-id (str error))
           (throw (Exception. (str error))))
         (try
           (let [response (request->response (get req :body) handler-fn context)
-                encoded (json/encode response)
                 send-fn (if (:error response) send-error send-response)]
-            (send-fn runtime-api lambda-runtime-aws-request-id encoded))
+            (send-fn runtime-api request-id response))
           (catch Exception e
-            (send-error runtime-api lambda-runtime-aws-request-id (str e)))))
+            (send-error runtime-api request-id (str e)))))
       (recur (get-lambda-invocation-request runtime-api)))))
